7941b002367224adff41bcaa21575c04b7b47cc3
ArtsMasonry <YOUR_GITHUB_EMAIL>
2025-09-23 07:58:17 -0500

    Documents: hard reset to stable tiles + filters page (no detail route yet)

--- src/app/documents/page.tsx
+++
@@ -1,4 +0,0 @@
-import DocumentsClient from "./DocumentsClient";
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-export default function DocumentsPage() { return <DocumentsClient />; }
